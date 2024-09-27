62ae3ccbd0edab6f4b0c030df5f1d8259ea52868
svash <s.vashkevych@gmail.com>
2024-09-27 19:51:05 +0300

    Search params settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
 import JobFilterSidebar from "@/components/JobFilterSidebar";
 import JobResults from "@/components/JobResults";
 import H1 from "@/components/ui/h1";
+import { JobFilterValues } from "@/lib/validation";
 
-export default async function Home() {
+interface PageProps {
+  searchParams: {
+    q?: string;
+    type?: string;
+    location?: string;
+    remote?: string;
+  };
+}
+
+export default async function Home({
+  searchParams: { q, location, remote, type },
+}: PageProps) {
+  const filterValues: JobFilterValues = {
+    q,
+    type,
+    location,
+    remote: remote === "true",
+  };
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
@@ -11,7 +29,7 @@ export default async function Home() {
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
         <JobFilterSidebar />
-        <JobResults />
+        <JobResults filterValues={filterValues} />
       </section>
     </main>
   );
