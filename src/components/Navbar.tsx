272146fbc83b57ed38dcfe10561e091203c933d9
svash <s.vashkevych@gmail.com>
2024-10-02 18:59:11 +0300

    Change logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
@@ -7,7 +7,13 @@ export default function Navbar() {
     <header className="shadow-sm">
       <nav className="m-auto flex max-w-5xl items-center justify-between px-3 py-5">
         <Link href="/" className="flex items-center gap-3">
-          <Image src={logo} width={40} height={40} alt="Logo" />
+          <Image
+            className="rounded-sm"
+            src={logo}
+            width={80}
+            height={80}
+            alt="Logo"
+          />
           <span className="text-xl font-black tracking-tight">DreamJob</span>
         </Link>
         <Button asChild>
